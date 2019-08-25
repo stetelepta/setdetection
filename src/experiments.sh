ef81614017c359eae62482c90cb0bc352f580f46
Salomon <s.tetelepta@gmail.com>
2019-08-25 17:48:02 +0200

    added Dockerfile and adjusted experiments for training on google cloud

--- src/experiments.sh
+++ src/experiments.sh
@@ -11,7 +11,12 @@
 #python ./src/find_best_layer.py --normalize="none" --model="mobilenet_v2" --nr_images=2430;
 #python ./src/find_best_layer.py --normalize="none" --model="mobilenet_v2" --nr_images=3240;
 
-python ./src/find_best_layer.py --normalize="minmax" --model="resnet" --nr_images=1620;
-python ./src/find_best_layer.py --normalize="standard" --model="resnet" --nr_images=1620;
-python ./src/find_best_layer.py --normalize="minmax" --model="resnet" --nr_images=2430;
-python ./src/find_best_layer.py --normalize="standard" --model="resnet" --nr_images=2430;
+#python ./src/find_best_layer.py --normalize="minmax" --model="resnet" --nr_images=1620;
+#python ./src/find_best_layer.py --normalize="standard" --model="resnet" --nr_images=1620;
+#python ./src/find_best_layer.py --normalize="minmax" --model="resnet" --nr_images=2430;
+#python ./src/find_best_layer.py --normalize="standard" --model="resnet" --nr_images=2430;
+
+
+python ./src/find_best_layer.py --normalize="none" --model="resnet" --nr_images=8100;
+python ./src/find_best_layer.py --normalize="none" --model="mobilenet" --nr_images=8100;
+python ./src/find_best_layer.py --normalize="none" --model="mobilenet_v2" --nr_images=8100;
